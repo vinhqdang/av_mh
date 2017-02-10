396296c63eba002d3b5deaf13482b866f907480d
Vinh Q. DANG <dqvinh87@gmail.com>
2017-02-10 15:00:12 +0100

    init

---
+++ roadshow.R
@@ -0,0 +1,15 @@
+train = read.csv("train.csv")
+test = read.csv("test.csv")
+
+train$Surge_Pricing_Type = as.factor(train$Surge_Pricing_Type)
+
+if (!require(h2o)) {
+  install.packages("h2o")
+}
+
+library (h2o)
+
+h2o.init()
+
+h_train = as.h2o(train)
+h_test = as.h2o (test)
